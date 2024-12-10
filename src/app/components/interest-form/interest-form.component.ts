3b6734c5182ae4fc0dc16e796911dc19a2aefe39
Daniel Badillo <daniel.sebastian.badillo@gmail.com>
2024-12-10 17:47:44 -0500

    form initializated

--- src/app/components/interest-form/interest-form.component.ts
+++ src/app/components/interest-form/interest-form.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-interest-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './interest-form.component.html',
-  styleUrl: './interest-form.component.scss'
+  styleUrl: './interest-form.component.scss',
 })
 export class InterestFormComponent {
-
+  interestForm: FormGroup = new FormGroup({});
+  constructor(private formBuilder: FormBuilder) {
+    this.interestForm = this.formBuilder.group({
+      initialInvestment: [''],
+      annualInvestment: [''],
+      expectedReturn: [''],
+      duration: [''],
+    });
+  }
 }
