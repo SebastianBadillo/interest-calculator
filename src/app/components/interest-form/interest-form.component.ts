7ec4729355f2d59700b7dfb1e13ea7b525ca856a
Daniel Badillo <daniel.sebastian.badillo@gmail.com>
2024-12-09 20:05:53 -0500

    Style basics, header and first component created

---
+++ src/app/components/interest-form/interest-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interest-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './interest-form.component.html',
+  styleUrl: './interest-form.component.scss'
+})
+export class InterestFormComponent {
+
+}
